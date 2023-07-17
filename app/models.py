15b73924ea1d4a092a8d581c20e93632ab6740c6
kanandev2024 <kananramazandev@gmail.com>
2023-07-17 17:34:39 +0300

    chnages

--- app/models.py
+++ app/models.py
@@ -17,7 +17,7 @@ class Student(models.Model):
     course = models.CharField(max_length=200, null=True, blank=True)
     year = models.CharField(max_length=200, null=True, blank=True)
     semester = models.CharField(max_length=200, null=True, blank=True)
-    unit = 
+    unit = models.CharField(max_length=200, blank=True, null=True)
     
     def __str__(self):
         return self.admission
