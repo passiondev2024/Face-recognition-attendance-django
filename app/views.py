3a9db667f021e7a82adb3b2983417eaefa80e672
kanandev2024 <kananramazandev@gmail.com>
2023-07-21 21:22:51 +0300

    updates

--- app/views.py
+++ app/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 # Create your views here.
 def Index(request):
     return render(request, 'app/index.html')
+
+def Attend(request):
+    return render(request, 'app/attend.html')
