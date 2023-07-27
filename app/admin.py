d8c0d8e5627fca900a0abb083c3466865b3ea327
kanandev2024 <kananramazandev@gmail.com>
2023-07-27 18:41:32 +0300

    chnages

--- app/admin.py
+++ app/admin.py
@@ -4,4 +4,4 @@ from .models import Student
 # Register your models here.
 @admin.register(Student)
 class StudentTable(admin.ModelAdmin):
-    list_display = ('user', 'firstname', 'lastname', 'phone', 'email', 'gender', 'profile_pic', 'school', 'department', 'course', 'year', 'semester', 'units')
+    list_display = ('user', 'first_name', 'last_name', 'phone', 'email', 'gender', 'profile_pic', 'school', 'department', 'course', 'year', 'semester', 'units')
