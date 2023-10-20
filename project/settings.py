0677bb5e17e15c13b68c5bb2e9b2718cbeda2457
kanandev2024 <kananramazandev@gmail.com>
2023-10-20 12:33:11 +0300

    changes

--- project/settings.py
+++ project/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, ('app/templates')),
+            BASE_DIR / 'templates',
             ],
         'APP_DIRS': True,
         'OPTIONS': {
