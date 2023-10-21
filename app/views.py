5ac171097828433b00bf674b93e7266680f8d1f1
kanandev2024 <kananramazandev@gmail.com>
2023-10-21 21:43:54 +0300

    changes

--- app/views.py
+++ app/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 
 
 # Create your views here.
+def Login(request):
+    return render(request, 'app/login.html')
+
 def Index(request):
     return render(request, 'app/index.html')
 
