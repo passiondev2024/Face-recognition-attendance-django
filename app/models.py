ce5320bc5e64e86e956cd00397bb03894d280c6e
kanandev2024 <kananramazandev@gmail.com>
2023-10-21 22:54:11 +0300

    changes

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Student(models.Model):
+    user = models.OneToOneField(User, null = True, blank = True, on_delete= models.CASCADE)
+    firstname = models.CharField(max_length=200, null=True, blank=True)
+    lastname = models.CharField(max_length=200, null=True, blank=True)
+    admission = models.CharField(max_length=13, null=True, blank=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    profile_pic = models.ImageField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.admission
