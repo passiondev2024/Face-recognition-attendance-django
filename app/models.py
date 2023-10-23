625404a6dcdce1932a6f8d2b314abda345fe639c
kanandev2024 <kananramazandev@gmail.com>
2023-10-23 17:24:22 +0300

    chnages

--- app/models.py
+++ app/models.py
@@ -7,7 +7,6 @@ class Student(models.Model):
     user = models.OneToOneField(User, null = True, blank = True, on_delete= models.CASCADE)
     firstname = models.CharField(max_length=200, null=True, blank=True)
     lastname = models.CharField(max_length=200, null=True, blank=True)
-    admission = models.CharField(max_length=13, null=True, blank=True)
     phone = models.CharField(max_length=200, null=True, blank=True)
     email = models.CharField(max_length=200, null=True, blank=True)
     gender = models.CharField(max_length=200, null=True, blank=True)
