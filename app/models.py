343f42a501962ff83981ac76758dd52e55b9b44b
kanandev2024 <kananramazandev@gmail.com>
2023-10-25 22:04:40 +0300

    changes

--- app/models.py
+++ app/models.py
@@ -8,9 +8,14 @@ class Student(models.Model):
     firstname = models.CharField(max_length=200, null=True, blank=True)
     lastname = models.CharField(max_length=200, null=True, blank=True)
     admission = models.CharField(max_length=13, null=True, blank=True)
-    phone = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True, blank=True)
+    email = models.CharField(max_length=200, null=True, blank=True)
+    gender = models.CharField(max_length=200, null=True, blank=True)
     profile_pic = models.ImageField(null=True, blank=True)
     
     def __str__(self):
         return self.admission
+    
+    
+# class MarkAttendance(models.Model):
+    
