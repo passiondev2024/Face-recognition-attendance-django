3be16cba378e83db9f6f77251d7a80b5b0646a6b
kanandev2024 <kananramazandev@gmail.com>
2023-10-26 20:26:21 +0300

    chnages

--- project/settings.py
+++ project/settings.py
@@ -128,3 +128,16 @@ STATICFILES_DIR = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from django.contrib.messages import constants as messages
+    MESSAGE_TAGS ={
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+    }
+except Exception as e:
+    pass
