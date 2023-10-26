666b20beda2a954e935ccf9cf82b54851803d8e3
kanandev2024 <kananramazandev@gmail.com>
2023-10-26 17:00:35 +0300

    chnages

--- app/static/js/chart.js
+++ app/static/js/chart.js
@@ -3,19 +3,13 @@ $(document).ready(function() {
 	// Bar Chart
 
 	var barChartData = {
-		labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+		labels: ['COM 110', 'COM2 10', 'COM 123', 'COM 452', 'COM 234'],
 		datasets: [{
 			label: 'Dataset 1',
 			backgroundColor: 'rgba(0, 158, 251, 0.5)',
 			borderColor: 'rgba(0, 158, 251, 1)',
 			borderWidth: 1,
 			data: [35, 59, 80, 81, 56, 55, 40]
-		}, {
-			label: 'Dataset 2',
-			backgroundColor: 'rgba(255, 188, 53, 0.5)',
-			borderColor: 'rgba(255, 188, 53, 1)',
-			borderWidth: 1,
-			data: [28, 48, 40, 19, 86, 27, 90]
 		}]
 	};
 
