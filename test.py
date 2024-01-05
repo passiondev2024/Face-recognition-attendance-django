ccedbc68659e38f7f720116b80b9e22d1bdd74b7
kanandev2024 <kananramazandev@gmail.com>
2024-01-05 17:01:35 +0300

    changes

--- test.py
+++ test.py
@@ -1,18 +1,26 @@
-import geocoder
+# import geocoder
 
-def get_current_gps_coordinates():
-    g = geocoder.ip('me')#this function is used to find the current information using our IP Add
-    if g.latlng is not None: #g.latlng tells if the coordiates are found or not
-        return g.latlng
-    else:
-        return None
+# def get_current_gps_coordinates():
+#     g = geocoder.ip('me')
+#     if g.latlng is not None:
+#         return g.latlng
+#     else:
+#         return None
 
-if __name__ == "__main__":
-    coordinates = get_current_gps_coordinates()
-    if coordinates is not None:
-        latitude, longitude = coordinates
-        print(f"Your current GPS coordinates are:")
-        print(f"Latitude: {latitude}")
-        print(f"Longitude: {longitude}")
-    else:
-        print("Unable to retrieve your GPS coordinates.")
+# if __name__ == "__main__":
+#     coordinates = get_current_gps_coordinates()
+#     if coordinates is not None:
+#         latitude, longitude = coordinates
+#         print(f"Your current GPS coordinates are:")
+#         print(f"Latitude: {latitude}")
+#         print(f"Longitude: {longitude}")
+#     else:
+#         print("Unable to retrieve your GPS coordinates.")
+
+import time
+from datetime import datetime
+from datetime import datetime, timedelta
+
+
+now = datetime.now()
+print(now.time())
