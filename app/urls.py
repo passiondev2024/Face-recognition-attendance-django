64e7ee3f8ef3f5829a16fcf3cd7d78ce51bdf368
kanandev2024 <kananramazandev@gmail.com>
2024-01-23 10:06:19 +0300

    changes

--- app/urls.py
+++ app/urls.py
@@ -13,4 +13,21 @@ urlpatterns = [
     path('editprofile',views.editProfile, name='editprofile'),
     path('change_password', views.ChangePassword, name='change_password'),
     path('logout', views.Logout, name='logout'),
+
+    # Reset Password
+    path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="app/passwordReset/forget-password.html"),
+     name="reset_password"),
+
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="app/passwordReset/password_reset_sent.html"), 
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="app/passwordReset/password_reset_form.html"), 
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="app/passwordReset/password_reset_done.html"), 
+        name="password_reset_complete"),
 ]
