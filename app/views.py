8fa2760050c80245be56e0d9eb50ba2549e36fca
kanandev2024 <kananramazandev@gmail.com>
2024-01-31 23:07:41 +0300

    changes

--- app/views.py
+++ app/views.py
@@ -113,7 +113,8 @@ def ProfilePic(request):
 def Index(request):
     logged_in_user = request.user
     student = Student.objects.get(user=logged_in_user)
-    units_list = student.units.split(',')
+    units_list_json = student.units.split(',')
+    units_list = [json.loads(unit_json) for unit_json in units_list_json]
     register = takeAttendance.objects.filter(student=student)
 
     # Annotate the QuerySet with attendance count per unit
