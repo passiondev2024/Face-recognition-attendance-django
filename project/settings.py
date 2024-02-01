8843a9c175963933e00a1cd4e275de68caffb555
kanandev2024 <kananramazandev@gmail.com>
2024-02-01 20:19:33 +0300

    changes

--- project/settings.py
+++ project/settings.py
@@ -136,6 +136,21 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'contactrongoshops@gmail.com'
+EMAIL_HOST_PASSWORD = 'qmzbskujimhmbpzh'
+EMAIL_USE_SSL = False
+
+
 try:
     from django.contrib.messages import constants as messages
     MESSAGE_TAGS ={
