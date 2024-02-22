72a3866364b3bf2f8b270ad7de38d65da2c6e04d
kanandev2024 <kananramazandev@gmail.com>
2024-02-22 16:56:51 +0300

    updates

--- app/views.py
+++ app/views.py
@@ -275,41 +275,25 @@ def get_current_gps_coordinates():
         return None
 
 
+from shapely.geometry import Point, Polygon
+
+from shapely.geometry import Point, Polygon
+
 def is_user_within_coordinates(user_latitude, user_longitude, room_coordinates):
     try:
         if len(room_coordinates) == 4:
-            # Assume a rectangle with 4 corner coordinates (top-left, top-right, bottom-right, bottom-left)
-            x, y = float(user_latitude), float(user_longitude)
+            # Assuming room_coordinates is a list of dictionaries with 'latitude' and 'longitude' keys
+            polygon_coordinates = [(float(coord['longitude']), float(coord['latitude'])) for coord in room_coordinates]
 
-            def is_point_inside_polygon(x, y, poly):
-                try:
-                    x, y = float(x), float(y)  # Convert x and y to float
+            user_point = Point(float(user_longitude), float(user_latitude))
+            room_polygon = Polygon(polygon_coordinates)
 
-                    n = len(poly)
-                    inside = False
-
-                    p1x, p1y = poly[0]
-                    for i in range(n + 1):
-                        p2x, p2y = poly[i % n]
-                        if y > min(p1y, p2y):
-                            if y <= max(p1y, p2y):
-                                if x <= max(p1x, p2x):
-                                    if p1y != p2y:
-                                        xinters = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
-                                        if p1x == p2x or x <= xinters:
-                                            inside = not inside
-                        p1x, p1y = p2x, p2y
-
-                    return inside
-
-                except Exception as e:
-                    print(f"Error checking user coordinates: {str(e)}")
-                    return False
-
+            return room_polygon.contains(user_point)
 
         else:
             print("Invalid number of room coordinates. Please provide exactly 4 coordinates.")
             return False
+
     except Exception as e:
         print(f"Error checking user coordinates: {str(e)}")
         return False
