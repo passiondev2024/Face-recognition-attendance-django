0ef9ae0f671f56f68777461f1ba372344c02d0a9
kanandev2024 <kananramazandev@gmail.com>
2024-02-28 17:09:34 +0300

    changes

--- test.py
+++ test.py
@@ -17,12 +17,10 @@
 #     else:
 #         print("Unable to retrieve your GPS coordinates.")
 
-import time
 from datetime import datetime
-from datetime import datetime, timedelta
-
 
 now = datetime.now()
 
+# Print the day of the week
 day_of_week = now.strftime("%A")
 print(day_of_week)
