7f68c176cb73d7ed8ff0b046482ba41120ee5167
kanandev2024 <kananramazandev@gmail.com>
2024-03-08 13:59:02 +0300

    updates

--- app/recognizer.py
+++ app/recognizer.py
@@ -31,8 +31,6 @@ def Recognizer(details, classNames):
     
     encodeListKnown = findEncondings(images)
     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    cap.set(3, 640)
-    cap.set(4, 480)
     
     while True:
         success, img = cap.read()
