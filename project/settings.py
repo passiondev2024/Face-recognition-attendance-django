2402cad8c8b4c84cfe03f0dec54b91bcfbecfc5a
kanandev2024 <kananramazandev@gmail.com>
2024-03-18 12:55:23 +0300

    updates

--- project/settings.py
+++ project/settings.py
@@ -127,6 +127,9 @@ STATIC_URL = "static/"
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, ('app/static')),
 ]
+STATIC_ROOT = [
+    os.path.join(BASE_DIR, 'staticfiles_builds', 'static')
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- project/urls.py
+++ project/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
 ]
 
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
