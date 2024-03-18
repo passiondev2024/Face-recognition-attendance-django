3d4b9688559a70a10d041bc5180f1ec80aad31db
kanandev2024 <kananramazandev@gmail.com>
2024-03-18 13:20:58 +0300

    updates

--- project/settings.py
+++ project/settings.py
@@ -134,8 +134,6 @@ STATIC_ROOT = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
